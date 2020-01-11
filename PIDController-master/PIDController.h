1c42bdcc076d9534265f6f5fdfa1e3ea232ba6d1
rowlavel <rowlavel@iu.edu>
2020-01-10 23:01:36 -0500

    added values for pid controller header

--- PIDController-master/PIDController.h
+++ PIDController-master/PIDController.h
@@ -4,23 +4,23 @@
 class PIDController{
 	public:
 		// variables
-		float epsilon_pitch;
-		float epsilon_roll;
-		float epsilon_yaw;
-		float epsilon_depth;
-		float dt; 
-		float Kp_pitch;
-		float Kd_pitch;
-		float Ki_pitch;
-		float Kp_roll;
-		float Kd_roll;
-		float Ki_roll;
-		float Kp_yaw;
-		float Kd_yaw;
-		float Ki_yaw;
-		float Kp_depth;
-		float Kd_depth;
-		float Ki_depth;
+		float epsilon_pitch = 0.01;
+		float epsilon_roll = 0.01;
+		float epsilon_yaw = 0.01;
+		float epsilon_depth = 0.01;
+		float dt = 0.01; 
+		float Kp_pitch = 0.1;
+		float Kd_pitch = 0.01;
+		float Ki_pitch = 0.005;
+		float Kp_roll = 0.1;
+		float Kd_roll = 0.01;
+		float Ki_roll = 0.005;
+		float Kp_yaw = 0.1;
+		float Kd_yaw = 0.01;
+		float Ki_yaw = 0.005;
+		float Kp_depth = 0.1;
+		float Kd_depth = 0.01;
+		float Ki_depth = 0.005;
 		
 		//functions
 		float PIDcal_pitch(float set_pitch, float actual_pitch);
